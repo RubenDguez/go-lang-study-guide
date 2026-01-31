af5d8f0bd849c10ea44949ca47c5faa1b8b9c57f
Ruben Dominguez <argenis.dominguez@hotmail.com>
2026-01-31 10:13:27 -0500

    add: maps go

--- 03-Go-Complex-Data-Structures-and-Memory-Allocation/03-Maps/main.go
+++ 03-Go-Complex-Data-Structures-and-Memory-Allocation/03-Maps/main.go
@@ -4,4 +4,38 @@ import "fmt"
 
 func main() {
 	fmt.Println("Maps")
+
+	grades := map[string]int{
+		"Ruben": 80,
+		"Lisa":  95,
+		"Alex":  91,
+	}
+
+	fmt.Printf("%+v\n", grades)
+	
+	ruben, ok := grades["Ruben"]
+	fmt.Println(ruben, ok)
+
+	student := "Ruben"
+	grade := 90
+	if _, ok := grades[student]; ok {
+		grades[student] = grade
+	}
+	fmt.Printf("%+v\n", grades)
+
+	student = "Lisa"
+	if val, ok := grades[student]; ok {
+		fmt.Printf("%s : %d\n", student, val)
+	}
+
+	student = "Alex"
+	delete(grades, student)
+	fmt.Printf("%+v\n", grades)
+
+	var configs = make(map[string]string)
+	fmt.Printf("%+v %T\n", configs, configs)
+	
+
+	configs["env"] = "neerqa"
+	fmt.Printf("%+v %T\n", configs, configs)
 }
