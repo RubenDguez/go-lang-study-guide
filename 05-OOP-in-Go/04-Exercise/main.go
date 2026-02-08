dc63b8eee20f5d485c6bf6bbb6ce4004d1c3d22a
RubenDguez <argenis.dominguez@hotmail.com>
2026-02-08 10:43:52 -0500

    update declaration and assigment poss;

--- 05-OOP-in-Go/04-Exercise/main.go
+++ 05-OOP-in-Go/04-Exercise/main.go
@@ -35,23 +35,23 @@ func main() {
 		Name:   "Ruben Dominguez",
 		Salary: 100000.00,
 	}
-	salariedEmployees = append(salariedEmployees, ruben)
-
+	
 	lisa := Employees.Commission{
 		Name:           "Lisa Freiwald",
 		BaseSalary:     30000.00,
 		Sales:          80000.00,
 		CommissionRate: 0.05,
 	}
-	commissionEmployees = append(commissionEmployees, lisa)
-
+	
 	alex := Employees.Hourly{
 		Name:        "Alexander Ramhit",
 		HourlyRate:  12.00,
 		HoursWorked: 120,
 	}
-	hourlyEmployees = append(hourlyEmployees, alex)
 
+	salariedEmployees = append(salariedEmployees, ruben)
+	commissionEmployees = append(commissionEmployees, lisa)
+	hourlyEmployees = append(hourlyEmployees, alex)
 
 	payroll := 0.00
 
